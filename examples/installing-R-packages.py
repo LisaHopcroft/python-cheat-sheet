ee5aca1141c84e551af536b64342c58319945661
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-08-23 12:24:00 +0100

    feat: example of installing R packages via rpy2

---
+++ examples/installing-R-packages.py
@@ -0,0 +1,13 @@
+### To install via rpy2
+from rpy2.robjects.vectors import StrVector
+import rpy2.robjects.packages as rpackages
+
+utils = rpackages.importr('utils')
+utils.chooseCRANmirror(ind=1)  # select the first mirror in the list
+packnames = ('zoo', 'caTools', 'gets')
+
+names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
+print(names_to_install)
+
+if len(names_to_install) > 0:
+    #utils.install_packages(StrVector(names_to_install))
